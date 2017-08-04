5ad3f7626f049e903530af04c32b2f67866af222
Ron Laniado <rlaniado24@gmail.com>
2017-08-03 22:25:59 -0400

    weatherUrl variable created
    The weatheUrl variable was created. It contains the API key neccessary in order for the DarkSky API to be called.

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-if (navigator.geolocation) {
-  //Get Latitude and Longitude
-  navigator.geolocation.getCurrentPosition(getPosition);
-  function getPosition(position) {
+$(document).ready( function() {
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(getPosition);
+    function getPosition(position) {
       var Longitude = position.coords.longitude;
       var Latitude = position.coords.latitude;
-  console.log(Longitude + " " + Latitude);
-}
-  console.log("Geolocation is working");
-} else {
-  alert("Geolocation is not supported on your browser.");
-}
+      console.log(Longitude + " " + Latitude);
+      var weatherUrl = "https://api.forecast.io/forecast/" + "94fc02d101411e20817bb1871a7dee34" + "/" + Longitude + "," + Latitude;
+      console.log(weatherUrl);
+  }
+      console.log("Geolocation is working");
+  } else {
+      alert("Geolocation is not supported on your browser.");
+  }
+
+  function getWeather() {
+  }
+})
+//Super Secret DarkSky API Key: 94fc02d101411e20817bb1871a7dee34
