7dc76f7dae188b84d25a0b593144e2af0807f369
Ron Laniado <rlaniado24@gmail.com>
2017-08-28 14:20:25 -0400

    No longer using the DarkSky API
    Javascript code has been reset; a new API will be used.

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-$(document).ready( function() {
-
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(getPosition);
-    function getPosition(position) {
-      var Longitude = position.coords.longitude;
-      var Latitude = position.coords.latitude;
-      console.log(Longitude + " " + Latitude);
-      var weatherUrl = "https://api.forecast.io/forecast/" + "94fc02d101411e20817bb1871a7dee34" + "/" + Longitude + "," + Latitude;
-      console.log(weatherUrl);
-  }
-      console.log("Geolocation is working");
-  } else {
-      alert("Geolocation is not supported on your browser.");
-  }
-
-  function getWeather() {
-  }
+$(document).ready(function() {
+  
 })
-//Super Secret DarkSky API Key: 94fc02d101411e20817bb1871a7dee34
