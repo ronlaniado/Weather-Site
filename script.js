c3f0ea145d9b30394baa7c7aa1241797e7630138
Ron Laniado <rlaniado24@gmail.com>
2017-08-29 11:24:49 -0400

    New API now properly works
    Javascript code has been rewritten, and the new API is ready to be utilized.

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+var celsius;
 $(document).ready(function() {
-  
-})
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var latitude = position.coords.latitude;
+      var longitude = position.coords.longitude;
+      var weatherUrl ="https://fcc-weather-api.glitch.me/api/current?lat=" + latitude + "5&lon=" + longitude;
+      $.ajax({
+        url: weatherUrl, success: function(data) {
+          var temp = data.main.temp;
+          var celsius = data.main.temp;
+          var city = data.name;
+          var country = data.sys.country;
+          var weather = data.weather[0].main;
+          console.log(data.main.temp);
+          console.log(data.name);
+          console.log(data.sys.country);
+          console.log(data.weather[0].main);
+        }
+      });
+    });
+  }
+});
