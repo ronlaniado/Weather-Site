b447706b21f5be88ba8d66cacc0d5ae095df5c55
Ron Laniado <rlaniado24@gmail.com>
2017-09-03 19:18:34 -0400

    Weather is now being displayed
    UI still ugly, but weather is finally being displayed, without the use of console.log()

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
-$(document).ready(function() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var latitude = position.coords.latitude;
-      var longitude = position.coords.longitude;
-      var weatherUrl ="https://fcc-weather-api.glitch.me/api/current?lat=" + latitude + "5&lon=" + longitude;
-      $.ajax({
-        url: weatherUrl, success: function(data) {
-          var temp = data.main.temp;
-          var city = data.name;
-          var country = data.sys.country;
-          var weather = data.weather[0].main;
-          console.log(data.main.temp);
-          console.log(data.name);
-          console.log(data.sys.country);
-          console.log(data.weather[0].main);
-        }
-      });
-    });
-  }
+var temp;
+$(document).ready(function () {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function (position) {
+			var latitude = position.coords.latitude;
+			var longitude = position.coords.longitude;
+			var weatherUrl = "https://fcc-weather-api.glitch.me/api/current?lat=" + latitude + "5&lon=" + longitude;
+			console.log(weatherUrl);
+
+			function showWeather() {
+				$.ajax({
+					url: weatherUrl,
+					success: function (data) {
+						temp = data.main.temp;
+						var city = data.name;
+						var country = data.sys.country;
+						var weather = data.weather[0].main;
+						var icon = data.weather.icon;
+						console.log(data.main.temp);
+						console.log(data.name);
+						console.log(data.sys.country);
+						console.log(data.weather[0].main);
+						console.log("showWeather() function is working");
+						$(".weather").empty("");
+						$("#temp").append(data.main.temp);
+						$("#city").append(data.name);
+						$("#country").append(data.sys.country);
+
+
+					}
+				});
+			}
+			showWeather();
+		});
+	}
 });
